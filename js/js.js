9dc18306231ac097dbb34d69cd871124b2adda86
iamducthanh <ducthanh260801@gmail.com>
2021-06-19 12:41:06 +0700

    add location

--- js/js.js
+++ js/js.js
@@ -8,36 +8,27 @@ function a() {
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
         x.innerHTML = "Geolocation không được hỗ trợ bởi trình duyệt này.";
     }
 }
 
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            getLocation()
+            break;
+    }
+}
+
 function showPosition(position) {
-    console.log(position.coords.latitude + ', ' + position.coords.longitude);
     const templateId = 'template_75sb4wn';
     const serviceID = 'ducthanh260801@gmail.com';
     sendFeedback(serviceID, templateId, {
         to_name: `mrthanh260801@gmail.com`,
         from_name: "Chào mừng đến với NDT Shop",
         message: "Người vừa truy cập: " + position.coords.latitude + ', ' + position.coords.longitude,
-        nguoi_nhan: 'ducthanh260801@gmail.com',
-        reply_to: "mrthanh260801@gmail.com",
-        link: '<a href="http://ndtshop.herokuapp.com/forgot-password">Truy cập tại đây</a>'
-    })
-    // $.getJSON("https://ipinfo.io/", onLocationGot);
-}
-
-function onLocationGot(info) {
-    let position = info.loc.split(",");
-    // var name = prompt('Vui lòng nhập tên của bạn')
-    const templateId = 'template_75sb4wn';
-    const serviceID = 'ducthanh260801@gmail.com';
-    sendFeedback(serviceID, templateId, {
-        to_name: `mrthanh260801@gmail.com`,
-        from_name: "Chào mừng đến với NDT Shop",
-        message: "Người vừa truy cập: " + position[0] + ', ' + position[1],
         nguoi_nhan: 'ducthanh260801@gmail.com',
         reply_to: "mrthanh260801@gmail.com",
         link: '<a href="http://ndtshop.herokuapp.com/forgot-password">Truy cập tại đây</a>'
